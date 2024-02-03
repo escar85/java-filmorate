1251c228e1365585ce884702f85146339f919771
Melnik Dmitry <escar1985@yandex.ru>
2024-02-03 19:30:50 +0300

    fix: checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface FilmStorage {
 
      List<Film> getAllFilms();
+
      Film createFilm(Film film);
 
      Film updateFilm(Film film);
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface UserStorage {
 
     List<User> getAllUsers();
+
     User createUser(User user);
 
     User updateUser(User user);
